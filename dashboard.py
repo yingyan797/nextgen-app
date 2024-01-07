225943ff7c1e02026fd25709ac8c6623aaadd4bc
Andy Chen <andy.chen121@imperial.ac.uk>
2024-01-07 20:49:02 +0000

    example webpage

--- dashboard.py
+++ dashboard.py
@@ -1,15 +1,14 @@
-from dotenv import load_dotenv
-import os
-from clarifai.client.model import Model
+from flask import Flask, render_template, request
+import imagegen
 
-load_dotenv()
-clarifai_pat = os.environ["CLARIFAI_PAT"]
+app = Flask(__name__)
 
-prompt = "Mongolian dance"
-inference_params = dict(quality="standard", size= '1024x1024')
-model_prediction = Model("https://clarifai.com/openai/dall-e/models/dall-e-3").predict_by_bytes(prompt.encode(), input_type="text", inference_params=inference_params)
-output_base64 = model_prediction.outputs[0].data.image.base64
+@app.route('/', methods=['GET', 'POST'])    # main page
+def index():
+    prompt = request.form.get('prompt')
+    if prompt:
+        imagegen.generate(prompt=prompt)
+    return render_template('index.html')
 
-f = open("result.png", "wb")
-f.write(output_base64)
-f.close()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
