a348080e2cf4a878582f24c742b8e7d97c351b05
Andy Chen <andy.chen121@imperial.ac.uk>
2024-01-10 14:13:45 +0000

    examples provided; save file/text destination

--- dashboard.py
+++ dashboard.py
@@ -10,18 +10,21 @@ def index():
     res = 0 # image 1 answer 2 audio 3
     ans = ""
     image_url = ""
+    dest = request.form.get("dest")
+    if not dest:
+        dest = ""
     if prompt:
         if request.form.get('image_gen'):
-            mo.image_gen(prompt=prompt)
+            mo.image_gen(prompt=prompt, dest=dest)
             res = 1
         elif request.form.get('ask'):
             img = request.form.get('img')
             if img:
                 image_url = "static/"+img
-            ans = mo.answer(prompt=prompt, image_url=image_url)
+            ans = mo.answer(prompt=prompt, image_url=image_url, dest=dest)
             res = 2
         elif request.form.get('tts'):
-            mo.text_speech(text=prompt)
+            mo.text_speech(text=prompt, dest=dest)
             res = 3
     print(res)
     return render_template('index.html', image_url=image_url, prompt=prompt, result=res, answer=ans)
--- models.py
+++ models.py
@@ -6,23 +6,36 @@ import base64
 load_dotenv()
 clarifai_pat = os.environ["CLARIFAI_PAT"]
 
-def image_gen(prompt="Mongolian dance", infparams=dict(quality="standard", size= '1024x1024'), dest="static/res_image.png"):
+def image_gen(prompt="Mongolian dance", infparams=dict(quality="standard", size= '1024x1024'), dest=""):
+    if not dest:
+        dest = "static/res_image.png"
+    else:
+        dest = "static/"+dest+".png"
     model_prediction = Model("https://clarifai.com/openai/dall-e/models/dall-e-3").predict_by_bytes(prompt.encode(), input_type="text", inference_params=infparams)
     output_base64 = model_prediction.outputs[0].data.image.base64
     f = open(dest, "wb")
     f.write(output_base64)
     f.close()
 
-def answer(prompt, image_url="", infparams=dict(temperature=0.2, max_tokens=100)):
+def answer(prompt, image_url="", infparams=dict(temperature=0.2, max_tokens=250), dest=""):
     if image_url:
         f = open(image_url, "rb")
         b64img = base64.b64encode(f.read()).decode('utf-8')
         f.close()
         infparams["image_base64"] = b64img
     model_prediction = Model("https://clarifai.com/openai/chat-completion/models/gpt-4-vision").predict_by_bytes(prompt.encode(), input_type="text", inference_params=infparams)
-    return model_prediction.outputs[0].data.text.raw
+    ans = model_prediction.outputs[0].data.text.raw
+    if dest:
+        f = open("static/"+dest+".txt", "a")
+        f.write("--Q: "+prompt+"-- "+image_url+'\n'+ans+"\n\n")
+        f.close()
+    return ans
 
-def text_speech(text, infparams=dict(voice="alloy", speed=1.0), dest="static/res_audio.mp3"):
+def text_speech(text, infparams=dict(voice="alloy", speed=1.0), dest=""):
+    if not dest:
+        dest = "static/res_audio.mp3"
+    else:
+        dest = "static/"+dest+".mp3"
     model_prediction = Model("https://clarifai.com/openai/tts/models/openai-tts-1").predict_by_bytes(text.encode(), input_type="text", inference_params=infparams)
     output_base64 = model_prediction.outputs[0].data.audio.base64
     f = open(dest, "wb")
